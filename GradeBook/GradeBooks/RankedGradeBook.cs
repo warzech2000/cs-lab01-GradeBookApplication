aaf2ad752193f2c656cc7b03815d03eb6e6fedbc
Warzech <warzech2000@gmail.com>
2022-04-02 16:04:09 +0200

    Task 6,7

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,5 +10,70 @@ namespace GradeBook.GradeBooks
         {
             base.Type = Enums.GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            int studentCount = Students.Count;
+            int gradeTop = 0;
+            if(studentCount < 5)
+            {
+                throw new InvalidOperationException();
+            }
+            else
+            {
+                foreach(Student student in Students)
+                {
+                    foreach(double grade in student.Grades)
+                    {
+                        if (grade > averageGrade) gradeTop++;
+                    }
+                }
+            }
+            if(gradeTop/studentCount < 1)
+            {
+                return 'A';
+            }
+            else if (gradeTop/studentCount < 2)
+            {
+                return 'B';
+            }
+            else if (gradeTop/studentCount < 3)
+            {
+                return 'C';
+            }
+            else if (gradeTop/studentCount < 4)
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
+        }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            }
+            else
+            {
+                base.CalculateStatistics();
+            }
+        }
+        public override void CalculateStudentStatistics(string name)
+        {
+
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            }
+            else
+            {
+                base.CalculateStudentStatistics(name);
+            }
+        }
     }
+    
 }
